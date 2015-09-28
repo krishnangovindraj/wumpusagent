08d86437daf3d6b74bdab32c8b43cfea36020526
R.Srinath <srinath132@gmail.com>
2015-09-28 23:43:24 +0530

    Second layout of my_agent.

--- my_agent.pl
+++ my_agent.pl
@@ -2,19 +2,31 @@
 
 init_agent:- % Populates our knowledge base with basic information ( location, orientation, arrowcount ).
 	writeln(init),
-	reset_agent. % Dummy
+	restart_agent. % Dummy
 
-reset_agent:- % Clears our knowledgeBase.
+restart_agent:- % Clears our knowledgeBase.
 	writeln(reset).
 
 
 % Dummy agent who just climbs
-simple_problem_solving_agent(Percepts, Action):-	
+simple_problem_solving_agent(Percepts, climb):-	
 	writeln(taking_action).
 
 
-update_state(Percept):-		% Takes the percept, adds to the knowledgebase
+record_percept([Stench,Breeze,Glitter,Bump,Scream],[X,Y],Orientation):-	% Takes the percept, adds to the knowledgebase
+	check_stench(Stench,[X,Y]),
+	check_breeze(Breeze,[X,Y]),
+	check_glitter(Glitter,[X,Y]),
+	check_bump(Bump,[X,Y],Orientation),
 	writeln(update_state).
+
+update_state([X,Y],Orientation,Action):-
+	Action=goforward, get_block_orientation([X,Y],Orientation,[X1,Y1]),asserta(cur_loc([X1,Y1])),asserta(cur_orient(Orientation));
+	Action=turnleft,left(Orientation,Orient2),asserta(cur_orient(Orient2));
+	Action=turnright,right(Orientation,Orient2),asserta(cur_orient(Orient2));
+	Action=grab,asserta(got_gold([X,Y]));
+	Action=shoot,assert(no_arrows);
+	Action=climb.
 
 search(Problem, Action):-	% Returns the action to take. 
 	writeln(search).
@@ -37,9 +49,9 @@ record_action(action):-		% Records whatever change occurs as a result of the act
 
 % Inference rules:
 %	OK([X,Y]) :- Tells you if [X,Y] is safe.
-%	aiming_at_wumpus(Location,Orientation):- Tells if youre facing the wumpus
+%	aiming_at_wumpus(Location,Orientation, facing):- Tells if youre facing the wumpus
 %	decide_action(Percept,Location,Orientation,Action):-	decides the action. Multiple clauses which determine what to do.
-%       faceorgo(Location, Action):- if facing the location, returns goforward; Else, rotate whichever way to face
+%       faceorgo(LocationStart, LocationFinal, Orientation, Action):- if facing the location, returns goforward; Else, rotate whichever way to face
 %       record_action(Action):-		% Records whatever change occurs as a result of the action.
 
 % Search Strategy:
@@ -52,4 +64,77 @@ record_action(action):-		% Records whatever change occurs as a result of the act
 %       7. faceorgo(predecessor(current_location))). % Backtrack!
 
 
+
+ok([X,Y]):-
+	no_pit([X,Y]),
+	no_wumpus([X,Y]).
+
+north([X,Y],[X1,Y1]):-
+	X1 is X,Y1 is Y+1.
+
+south([X,Y],[X1,Y1]):-
+	X1 is X,Y1 is Y-1.
+
+east([X,Y],[X1,Y1]):-
+	X1 is X+1,Y1 is Y.
+
+west([X,Y],[X1,Y1]):-
+	X1 is X-1,Y1 is Y.
+
+left(Orient1,Orient2):
+	Orient1=eastd, Orient2 is northd;
+	Orient1=westd, Orient2 is southd;
+	Orient1=northd, Orient2 is westd;
+	Orient1=southd, Orient2 is eastd.
+
+right(Orient1,Orient2):
+	Orient1=eastd, Orient2 is southd;
+	Orient1=westd, Orient2 is northd;
+	Orient1=northd, Orient2 is eastd;
+	Orient1=southd, Orient2 is westd.
+
+check_stench(no,[X,Y]):-
+	north([X,Y],[X1,Y1]),
+	assert(no_wumpus([X1,Y1])),
+	south([X,Y],[X2,Y2]),
+	assert(no_wumpus([X2,Y2])),
+	east([X,Y],[X3,Y3]),
+	assert(no_wumpus([X3,Y3])),
+	west([X,Y],[X4,Y4]),
+	assert(no_wumpus([X4,Y4])).
+
+check_stench(yes,[X,Y]):-
+	.
+
+check_breeze(no,[X,Y]):-
+	north([X,Y],[X1,Y1]),
+	assert(no_pit([X1,Y1])),
+	south([X,Y],[X2,Y2]),
+	assert(no_pit([X2,Y2])),
+	east([X,Y],[X3,Y3]),
+	assert(no_pit([X3,Y3])),
+	west([X,Y],[X4,Y4]),
+	assert(no_pit([X4,Y4])).
+
+check_breeze(yes,[X,Y]):-
+	.
+
+check_glitter(yes,[X,Y]):-
+	assert(got_gold([X,Y])).
+
+check_glitter(no, [X,Y]):-
+	.
+
+get_block_orientation([X,Y],Orientation,[X1,Y1]):
+	Orientation = eastd, east([X,Y],[X1,Y1]);
+	Orientation = westd, west([X,Y],[X1,Y1]);
+	Orientation = northd, north([X,Y],[X1,Y1]);
+	Orientation = southd,  south([X,Y],[X1,Y1]).
+
+check_bump(yes,[X,Y], Orientation):-
+	get_block_orientation([X,Y],Orientation,[X1,Y1]),
+	assert(out_of_bound([X1,Y1])).
+
+check_bump(no,[X,Y], Orientation):-
+	.
 
